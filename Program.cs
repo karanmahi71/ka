8433fe411ada44dd18537edb9643b2aefc09d97a
karanmahi71 <karankasbe7171@gmail.com>
2022-07-29 00:05:04 +0530

    [karan] added some file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 10;
+
+Console.WriteLine(num);
+long num2 = 13;
+Console.WriteLine(num2);
+//12.24
+double num3 = 12.24;
+Console.WriteLine(num3);
+float num4 = 12.1234567890f;
+//karan
+string name = "karan";
+Console.WriteLine(name);
+//A b c
+char data = 'A';
+Console.WriteLine(data);
+//hello
+Console.WriteLine("karan");
+string userName= Console.ReadLine();
+Console.WriteLine("hello + userName");
+//concat
+Console.WriteLine("hello {0}",userName);
